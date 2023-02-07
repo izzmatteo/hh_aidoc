8adc3e28d2379104147d1187e9d1cc3a571d26d3
izzmatteo <83980937+izzmatteo@users.noreply.github.com>
2023-02-07 17:00:10 +0100

    Update client.lua

--- hh_aidoc[ESX]/client.lua
+++ hh_aidoc[ESX]/client.lua
@@ -1,19 +1,17 @@
-local ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-	   TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-	   Citizen.Wait(200)
-	end
-end)
-
+ESX = nil
 local Active = false
 local test = nil
 local test1 = nil
+local Act = true
 local spam = true
-
 local isDead = false
 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(200)
+    end
+end)
 
 AddEventHandler('playerSpawned', function(spawn)
 	isDead = false
@@ -21,30 +19,33 @@ end)
 
 AddEventHandler('esx:onPlayerDeath', function(data)
 	isDead = true
-end)	
+end)
 
 
-
-RegisterCommand("help", function(source, args, raw)
-	if isDead and spam then
-		ESX.TriggerServerCallback('hhfw:docOnline', function(EMSOnline, hasEnoughMoney)
-
-			if EMSOnline <= Config.Doctor and hasEnoughMoney and spam then
-				SpawnVehicle(GetEntityCoords(PlayerPedId()))
-				TriggerServerEvent('hhfw:charge')
-				Notify("Medic is arriving")
-			else
-				if EMSOnline > Config.Doctor then
-					Notify("There is too many medics online", "error")
-				elseif not hasEnoughMoney then
-					Notify("Not Enough Money")
-				else
-					Notify("Wait Paramadic is on its Way")
-				end	
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if isDead and spam then
+			if IsControlJustPressed(1, 47) then
+				ESX.TriggerServerCallback('hhfw:docOnline', function(EMSOnline, hasEnoughMoney)
+					if EMSOnline <= Config.Doctor and hasEnoughMoney and spam then
+						SpawnVehicle(GetEntityCoords(PlayerPedId()))
+						TriggerServerEvent('hhfw:charge')
+						ShowAdvNotification("CHAR_PA_FEMALE", "Doctor", "", "There is no EMS in the city don't worry i will send my PA for you!")
+						--Notify("Medic is arriving")
+					else
+						if EMSOnline > Config.Doctor then
+							exports['CRP_Notify']:Alert("Doctor", "We got your signal, please wait", 5000, 'bell')
+							--Notify("There is too many medics online", "error")
+						elseif not hasEnoughMoney then
+							Notify("Not Enough Money")
+						else
+							Notify("Wait Paramadic is on its Way")
+						end	
+					end
+				end)
 			end
-		end)
-	else
-		Notify("This can only be used when dead")
+		end
 	end
 end)
 
@@ -62,7 +63,7 @@ function SpawnVehicle(x, y, z)
 	while not HasModelLoaded('s_m_m_doctor_01') do
 		Wait(1)
 	end
-	local spawnRadius = 40                                                    
+	local spawnRadius = 100                                                    
     local found, spawnPos, spawnHeading = GetClosestVehicleNodeWithHeading(loc.x + math.random(-spawnRadius, spawnRadius), loc.y + math.random(-spawnRadius, spawnRadius), loc.z, 0, 3, 0)
 
 	if not DoesEntityExist(vehhash) then
@@ -72,6 +73,8 @@ function SpawnVehicle(x, y, z)
 		SetVehicleNumberPlateText(mechVeh, "HHFW")
 		SetEntityAsMissionEntity(mechVeh, true, true)
 		SetVehicleEngineOn(mechVeh, true, true, false)
+		SetVehicleSiren(mechVeh, true)
+
         
         mechPed = CreatePedInsideVehicle(mechVeh, 26, GetHashKey('s_m_m_doctor_01'), -1, true, false)              	
         
@@ -86,31 +89,32 @@ function SpawnVehicle(x, y, z)
 		test = mechVeh
 		test1 = mechPed
 		Active = true
+
+		Citizen.CreateThread(function()
+			while true do
+			  Citizen.Wait(200)
+				if Active or Act then
+					local loc = GetEntityCoords(GetPlayerPed(-1))
+					local lc = GetEntityCoords(test)
+					local ld = GetEntityCoords(test1)
+					local dist = Vdist(loc.x, loc.y, loc.z, lc.x, lc.y, lc.z)
+					local dist1 = Vdist(loc.x, loc.y, loc.z, ld.x, ld.y, ld.z)
+					if dist <= 20 then
+						if Active then
+							TaskGoToCoordAnyMeans(test1, loc.x, loc.y, loc.z, 1.0, 0, 0, 786603, 0xbf800000)
+						end
+						if dist1 <= 1 then 
+							Active = false
+							Act = false
+							ClearPedTasksImmediately(test1)
+							DoctorNPC()
+						end
+					end
+				end
+			end
+		end)
     end
 end
-
-Citizen.CreateThread(function()
-    while true do
-      Citizen.Wait(200)
-        if Active then
-            local loc = GetEntityCoords(GetPlayerPed(-1))
-			local lc = GetEntityCoords(test)
-			local ld = GetEntityCoords(test1)
-            local dist = Vdist(loc.x, loc.y, loc.z, lc.x, lc.y, lc.z)
-			local dist1 = Vdist(loc.x, loc.y, loc.z, ld.x, ld.y, ld.z)
-            if dist <= 10 then
-				if Active then
-					TaskGoToCoordAnyMeans(test1, loc.x, loc.y, loc.z, 1.0, 0, 0, 786603, 0xbf800000)
-				end
-				if dist1 <= 1 then 
-					Active = false
-					ClearPedTasksImmediately(test1)
-					DoctorNPC()
-				end
-            end
-        end
-    end
-end)
 
 
 function DoctorNPC()
@@ -120,11 +124,10 @@ function DoctorNPC()
 	end
 
 	TaskPlayAnim(test1, "mini@cpr@char_a@cpr_str","cpr_pumpchest",1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
-	--exports['progressBars']:startUI(Config.ReviveTime, "The doctor is giving you medical aid")
 	TriggerEvent("mythic_progbar:client:progress", {
-        name = "ai_doc",
-        duration = Config.ReviveTime,
-        label = "The doctor is giving you medical aid",
+        name = "unique_action_name",
+        duration = 10000,
+        label = "Giving CPR.",
         useWhileDead = true,
         canCancel = false,
         controlDisables = {
@@ -132,24 +135,32 @@ function DoctorNPC()
             disableCarMovement = true,
             disableMouse = false,
             disableCombat = true,
-        },
-        animation = {},
-        prop = {}
+        }
     }, function(status)
         if not status then
-        ClearPedTasks(test1)
-	Citizen.Wait(500)
-	TriggerEvent('esx_ambulancejob:revive')
-	StopScreenEffect('DeathFailOut')	
-	Notify("Your treatment is done, you were charged: "..Config.Price)
-	RemovePedElegantly(test1)
-	DeleteEntity(test)
-	spam = true
+            -- Do Something If Event Wasn't Cancelled
         end
-    end)	
+    end)
+	Wait(Config.ReviveTime)
+	ClearPedTasks(test1)
+	Citizen.Wait(500)
+	TriggerEvent('esx_ambulancejob:revive', GetPlayerServerId(PlayerId()))
+	ShowAdvNotification("CHAR_PA_FEMALE", "Doctor", "", "Your treatment is done, you were charged : ~g~ $"..Config.Price)
+	--Notify("Your treatment is done, you were charged: "..Config.Price)
+	Citizen.Wait(5000)
+	DeletePed(test1)
+	DeleteVehicle(test)
+	spam = true
 end
 
 
-function Notify(msg)
+function Notify(msg, state)
     ESX.ShowNotification(msg)
+end
+
+function ShowAdvNotification(image, title, subtitle, text)
+	SetNotificationTextEntry("STRING");
+	AddTextComponentString(text);
+	SetNotificationMessage(image, image, false, 0, title, subtitle);
+	DrawNotification(false, true);
 end
